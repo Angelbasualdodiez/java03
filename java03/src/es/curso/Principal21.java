2ccc5cdfe09592a9380da9e5f52d739f857ec064
cie <cie@DESKTOP-U2G3M5P>
2024-11-29 13:20:45 +0100

    hoy

---
+++ java03/src/es/curso/Principal21.java
@@ -0,0 +1,13 @@
+package es.curso;
+//sacar la posicion de la x
+public class Principal21 {
+	public static void main(String[] args) {
+		String cadena="hola que x tal estas";
+		int contador=0;
+		for(int i=0;i<cadena.length();i++) {
+			if(cadena.charAt(i)=='x'){
+				System.out.println(i);			
+	}		
+}
+	}
+}
